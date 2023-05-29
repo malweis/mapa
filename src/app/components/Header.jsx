7bbdb30f1a1c7e2c52fdcd56efeb4a75cab8d76d
malweis <58945171+malweis@users.noreply.github.com>
2023-05-29 12:21:58 -0400

    feat:solo mapa

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import CustomButton from './CustomButtons'
+
+function Header() {
+  return (
+    <header className="text-gray-600 body-font  heads w-full border-4 border-red-600 z-90">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+            
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base text-white justify-center">
+          <a className="mr-5 hover:text-gray-900">First Link</a>
+          <a className="mr-5 hover:text-gray-900">Second Link</a>
+          <a className="mr-5 hover:text-gray-900">Third Link</a>
+          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+        </nav>
+        <CustomButton
+      bgColor="bg-white"
+      textColor="text-blue-500"
+      lineColor="border-black"
+      sideImage={  <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-6 h-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>}
+      opacity="opacity-1"
+    >
+      Click Me
+    </CustomButton>
+      </div>
+    </header>
+  )
+}
+
+export default Header

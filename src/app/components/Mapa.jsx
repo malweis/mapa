d1b8d16fcedb1baa5aa6775777ae68efd7338a70
malweis <58945171+malweis@users.noreply.github.com>
2023-05-30 09:00:19 -0400

    fix!: añade use client a todos los componentes
    BREAKING CHANGE: puede ser que rompa la generacion del mapa

--- src/app/components/Mapa.jsx
+++ src/app/components/Mapa.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useRef, useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet';
 import L from 'leaflet';

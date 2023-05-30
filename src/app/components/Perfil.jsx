79b69520e99920a0995ac261ee81ad09c0a880db
malweis <58945171+malweis@users.noreply.github.com>
2023-05-30 15:32:22 -0400

    feat: Perfil mapeado a un array, Card con estilos, Recover con estilos

--- src/app/components/Perfil.jsx
+++ src/app/components/Perfil.jsx
@@ -1,23 +1,56 @@
 "use client"
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BadgeIcon from '@mui/icons-material/Badge';
 import PersonIcon from '@mui/icons-material/Person';
 import Link from 'next/link';
+import axios from 'axios';
 
 
 
 
 function Perfil() {
 
+  const [data, setData] = useState([]);
+
+  
+const fakeData = [
+  {
+    id: 1,
+    nombre: "John Doe",
+    email: "johndoe@example.com",
+    ultima_fecha: "2022-05-30",
+    sexo: "Male",
+    fecha_nacimiento: "1990-01-01",
+    ci: "12345678"
+  },
+  // Add more fake data objects as needed
+];
+
+  useEffect(() => {
+    setData(fakeData);
+  }, []);
+
+  // const getAndSetData = () => {
+  //   axios.get('http://192.168.16.90:8000/api/solicitudes')
+  //     .then(response => {
+  //       console.log('API request succeeded');
+  //       console.log(response.data.data);
+  //       setData(response.data.data);
+  //     })
+  //     .catch(error => {
+  //       console.log('API request failed');
+  //       console.log(error);
+  //     });
+  // };
+
+
 
 
 
   return (
     <div className='perfil flex flex-col gap-4 justify-center items-center border-2 border-red-500'>
-        <div
-          className="bg-white w-[250px] rounded-xl shadow"
-          aria-label="card-overlay-v3"
-        >
+        {data.map((fakeItem) => (
+        <div className="bg-white w-[250px] rounded-xl shadow" aria-label="card-overlay-v3" key={fakeItem.id}>
           <div className="w-full rounded-xl h-[250px] flex-shrink-0">
             <img
               src="https://plus.unsplash.com/premium_photo-1682309704250-6bac0f499665?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1512&q=80"
@@ -27,29 +60,28 @@ function Perfil() {
           </div>
           <div className="flex flex-col flex-1 p-5">
             <div className="pb-5 mb-5 border-b border-gray-200">
-              <h3 className="mb-1 text-lg font-bold">Alan Toro</h3>
-              <span className="text-sm">alan@gmail.com </span><br/>
-              <span className="text-sm">Ultima donacion: 2122-32-33</span>
+              <h3 className="mb-1 text-lg font-bold">{fakeItem.nombre}</h3>
+              <span className="text-sm">{fakeItem.email}</span><br/>
+              <span className="text-sm">Ultima donacion: {fakeItem.ultima_fecha}</span>
             
-                <PersonIcon className="text-red-600" />
-                <span className="text-sm ">Masculino</span>
-             
+              <PersonIcon className="text-red-600" />
+              <span className="text-sm ">{fakeItem.sexo}</span>
             </div>
             <div className="flex items-center justify-between w-full ml-auto">
-              <div className="text-sm text-slate-400">1999-10-12</div>
+              <div className="text-sm text-slate-400">{fakeItem.fecha_nacimiento}</div>
               <div className="flex items-center gap-x-1">
                 <BadgeIcon className="text-red-600" />
-                <span className="text-sm font-bold">1231231</span>
+                <span className="text-sm font-bold">{fakeItem.ci}</span>
               </div>
             </div>
           </div>
-          
         </div>
-        <button class="text-white w-[250px] bg-red-600 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Editar Información</button>
+      ))}
+        <button className="text-white w-[250px] bg-red-600 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Editar Información</button>
              
-        <button class="text-white w-[250px] bg-red-600 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"><Link href={'/Recover'} >Cambiar contraseña</Link></button>
+        <button className="text-white w-[250px] bg-red-600 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"><Link href={'/Recover'} >Cambiar contraseña</Link></button>
            
-        <button class="text-white w-[250px] bg-red-600 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Cerrar sesión</button>
+        <button className="text-white w-[250px] bg-red-600 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Cerrar sesión</button>
             
     </div>
   )
--- src/app/components/Recover.jsx
+++ src/app/components/Recover.jsx
@@ -35,14 +35,14 @@ function Recover() {
   return (
     <div className='recover flex justify-center items-center'>
          
-        <section class="text-gray-600 body-font bg-gray-100 rounded-lg">
-          <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
-            <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-              <h1 class="title-font font-medium text-3xl text-gray-900">Restablece tu contraseña para acceder a tu cuenta.</h1>
+        <section className="text-gray-600 body-font bg-gray-100 rounded-lg">
+          <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+            <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+              <h1 className="title-font font-medium text-3xl text-gray-900">Restablece tu contraseña para acceder a tu cuenta.</h1>
              
             </div>
-            <div class="lg:w-2/6 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-              <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Ingresa tu correo</h2>
+            <div className="lg:w-2/6 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+              <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Ingresa tu correo</h2>
              
               <div className="flex flex-col items-start mb-5 gap-y-3">
           <label htmlFor="recover_email" className="text-sm font-medium cursor-pointer">
@@ -60,8 +60,8 @@ function Recover() {
           />
           {emailError && <p className="text-red-500">{emailError}</p>}
         </div>
-              <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Enviar</button>
-              <p class="text-xs text-gray-500 mt-3">Enviar Solicitud de recuperacion</p>
+              <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Enviar</button>
+              <p className="text-xs text-gray-500 mt-3">Enviar Solicitud de recuperacion</p>
             </div>
           </div>
         </section>

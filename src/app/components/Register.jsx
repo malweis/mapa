5bfafd7d0570d397902753183e554c97bf051837
malweis <58945171+malweis@users.noreply.github.com>
2023-06-01 17:01:29 -0400

    feat: Añade inputs contraseñas React-aria

--- src/app/components/Register.jsx
+++ src/app/components/Register.jsx
@@ -193,138 +193,136 @@ function Register() {
             </div>
 
 
-        <div className="flex flex-col items-start mb-5 gap-y-3">
-          <label htmlFor="password" className="text-sm font-medium cursor-pointer">
-            Contraseña
-          </label>
-          <div className="relative w-full">
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              className="w-full p-4 pr-12 transition-all bg-transparent border rounded-lg outline-none border-slate-200 focus:border-blue-500"
-              placeholder="Ingresa tu contraseña"
-              value={password}
-              onChange={handlePasswordChange}
-              onBlur={validatePassword}
-            />
 
-          
-
-            <span
-              className="absolute cursor-pointer text-slate-400 right-4 top-2/4 -translate-y-2/4"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {/* Password visibility toggle icons */}
-              {showPassword ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+            <div className="flex flex-col items-start ">
+              <div className="relative w-full">
+                <Texto
+                        label="Contraseña"
+                        tipo={showPassword ? "text" : "password"}
+                        id="password"
+                        validation={passwordError ? 'invalid' : 'valid'}
+                        error={passwordError}
+                        required={true}
+                        mode="text"
+                        placeholder="Ingresa tu contraseña"
+                        evento={handlePasswordChange}
+                        eventoblur={validatePassword}
+                      />
+                         <span
+                  className="absolute cursor-pointer text-slate-400 right-4 top-8/12 -translate-y-[3.8em]"
+                  onClick={() => setShowPassword(!showPassword)}
                 >
-                   <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                        />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                   <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-                        />
-                </svg>
-            
-              )}
-            </span>
-            
-          </div>
-            {/* Password validation error */}
+                  {/* Password visibility toggle icons */}
+                  {showPassword ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                       <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                            />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                       <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                            />
+                    </svg>
+                
+                  )}
+                </span>
+              </div>
+                  {/* Password validation error */}
             {passwordError && <p className="text-red-500">{passwordError}</p>}
-        </div>
-
-        <div className="flex flex-col items-start mb-5 gap-y-3">
-          <label htmlFor="confirmPassword" className="text-sm font-medium cursor-pointer">
-            Confirmar contraseña
-          </label>
-          <div className="relative w-full">
-            <input
-              type={showPassword ? "text" : "password"}
-              name="confirmPassword"
-              className="w-full p-4 pr-12 transition-all bg-transparent border rounded-lg outline-none border-slate-200 focus:border-blue-500"
-              placeholder="Repite tu constraseña"
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-              onBlur={validateConfirmPassword}
-            />
-
-        
-
-            <span
-              className="absolute cursor-pointer text-slate-400 right-4 top-2/4 -translate-y-2/4"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {/* Password visibility toggle icons */}
-              {showPassword ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                   <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                        />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                   <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-                        />
-                </svg>
+            </div>
             
-              )}
-            </span>
-          </div>
-              {/* Confirm password validation error */}
-              {confirmPasswordError && <p className="text-red-500">{confirmPasswordError}</p>}
-        </div>
+
+            <div className="flex flex-col items-start ">
+              <div className="relative w-full">
+                <Texto
+                        label="Confirma tu contraseña"
+                        tipo={showPassword ? "text" : "password"}
+                        id="confirmPassword"
+                        validation={passwordError ? 'invalid' : 'valid'}
+                        error={passwordError}
+                        required={true}
+                        mode="text"
+                        placeholder="Ingresa tu contraseña"
+                        evento={handleConfirmPasswordChange}
+                        eventoblur={validateConfirmPassword}
+                      />
+                         <span
+                  className="absolute cursor-pointer text-slate-400 right-4 top-8/12 -translate-y-[3.8em]"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {/* Password visibility toggle icons */}
+                  {showPassword ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                       <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                            />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                       <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                            />
+                    </svg>
+                
+                  )}
+                </span>
+              </div>
+                  {/* Password validation error */}
+                  {confirmPasswordError && <p className="text-red-500">{confirmPasswordError}</p>}
+            </div>
+               
+       
+        
 
         <div className="flex flex-col items-start mb-5 gap-y-3">
           <label htmlFor="Fecha" className="text-sm font-medium cursor-pointer">
